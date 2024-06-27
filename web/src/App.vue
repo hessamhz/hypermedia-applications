8c69d13331b8b0a59ebba2cb0c001d5e87c30c05
Vi <37715529+gingervi@users.noreply.github.com>
2024-06-27 15:20:37 +0200

    added web folder with first web design content

---
+++ web/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import NavBar from "./components/Navbar.vue"
+import Hero from "./components/Hero.vue"
+import Centre from "./components/Center.vue"
+import Story from "./components/Story.vue"
+import Mission from "./components/Mission.vue"
+import Statistics from "./components/Statistics.vue"
+
+</script>
+
+<template>
+  
+
+  <main>
+    <body class="bg-white h-screen">
+      <header>
+        <NavBar />
+        
+      </header>
+
+      <Hero />
+      <Centre />
+      <Story />
+      <Mission />
+      <Statistics />
+    </body>
+    
+     
+  </main>
+</template>
+
---
+++ web/src/components/Center.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="flex flex-col items-center border-2 bg-[url('../assets/ourcenter.png')] h-[36rem] bg-center bg-cover bg-fixed bg-no-repeat py-36 px-32 gap-16">
+        <h1 class="font-bold text-7xl text-white">Our Center</h1>
+        <p class="font-semibold text-4xl text-center text-white">Lorem ipsum dolor sit amet consectetur. Sit quam ac pellentesque aenean ante. At dolor elementum mi mollis. Cras facilisis tellus arcu commodo arcu et. Sodales amet massa duis sed condimentum justo tortor in tristique.</p>
+    </div>
+</template>
