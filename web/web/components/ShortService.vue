2ca1b4834de286f70e5dfc160fd77d883771fd2a
Vi <37715529+gingervi@users.noreply.github.com>
2024-07-05 01:10:16 +0200

    added services page + minor fixes to some components

---
+++ web/web/components/ShortService.vue
@@ -0,0 +1,37 @@
+<script setup>
+    defineProps(['name', 'schedule',  'description'])
+</script>
+
+<template>
+    <div class="flex flex-row border-2 items-center justify-center w-full mt-8 gap-3">
+        <div class="flex items-center justify-center w-[10rem] h-[10rem] bg-[#D7A549] bg-opacity-20 rounded-xl">
+            <img src="#">
+        </div>
+        <div class="flex flex-col items-start justify-top px-10 py-6 bg-[#D7A549] bg-opacity-20 w-[40rem] h-[10rem] rounded-xl ">
+            <a class="font-semibold text-xl" href="/service"> {{ name }} </a>
+            <p class="font-medium text-base text-gray-700"> {{ schedule }} </p>
+            <p class="font-medium text-base"> {{ description }} </p>
+        </div>
+    </div>
+    <div class="flex flex-row border-2 items-center justify-center w-full mt-3 gap-3">
+        <div class="flex flex-col items-start justify-top px-10 py-6 bg-[#7A4194] bg-opacity-20 w-[40rem] h-[10rem] rounded-xl ">
+            <h3 class="font-semibold text-xl"> {{ name }} </h3>
+            <p class="font-medium text-base text-gray-700"> {{ schedule }} </p>
+            <p class="font-medium text-base"> {{ description }} </p>
+        </div>
+        <div class="flex items-center justify-center w-[10rem] h-[10rem] bg-[#7A4194] bg-opacity-20 rounded-xl">
+            <img src="#">
+        </div>
+    </div>
+    <div class="flex flex-row border-2 items-center justify-center w-full mt-3 gap-3">
+        <div class="flex items-center justify-center w-[10rem] h-[10rem] bg-[#D7A549] bg-opacity-20 rounded-xl">
+            <img src="#">
+        </div>
+        <div class="flex flex-col items-start justify-top px-10 py-6 bg-[#D7A549] bg-opacity-20 w-[40rem] h-[10rem] rounded-xl ">
+            <h3 class="font-semibold text-xl"> {{ name }} </h3>
+            <p class="font-medium text-base text-gray-700"> {{ schedule }} </p>
+            <p class="font-medium text-base"> {{ description }} </p>
+        </div>
+    </div>
+</template>
+
---
+++ web/web/pages/services.vue
@@ -0,0 +1,6 @@
+<template>
+    <PageTitle title="Our Services" />
+    <div>
+        <ShortService name="Service1" schedule="Mon - Fri 18:30-20:30" description="a beutiful service like this" />
+    </div>
+</template>
