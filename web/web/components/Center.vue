e717a6e7946e5702802748742ce484aef9866983
Vi <37715529+gingervi@users.noreply.github.com>
2024-07-05 15:19:57 +0200

    added buttons interactions on index page and navbar

--- web/web/components/Center.vue
+++ web/web/components/Center.vue
@@ -2,6 +2,6 @@
     <div class="flex flex-col justify-center items-center bg-[url('~/assets/ourcenter.png')] h-[28rem] bg-center bg-cover bg-fixed bg-no-repeat px-64 gap-16">
         <h1 class="font-bold text-5xl text-white">Our Center</h1>
         <p class="font-medium text-2xl text-center text-white">Lorem ipsum dolor sit amet consectetur. Sit quam ac pellentesque aenean ante. At dolor elementum mi mollis. Cras facilisis tellus arcu commodo arcu et. Sodales amet massa duis sed condimentum justo tortor in tristique.</p>
-        <button class="bg-white  w-[12rem] h-[2.5rem] rounded-full font-semibold">Discover The Hive</button>
+        <button @click="navigateTo({ path: '/about' })" class="bg-white  w-[12rem] h-[2.5rem] rounded-full font-semibold">Discover The Hive</button>
     </div>
 </template>
