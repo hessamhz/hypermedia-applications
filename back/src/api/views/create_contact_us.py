8d2e0bd828445beb0cf1b4b187ff515ed445329f
Hessam Hashemizadeh <hessam.hashemi21@gmail.com>
2024-07-12 18:19:23 +0200

    fix: contact us csrf excempt

--- back/src/api/views/create_contact_us.py
+++ back/src/api/views/create_contact_us.py
@@ -5,7 +5,10 @@ from src.apps.website.models import ContactUs
 from src.apps.website.serializers import ContactUsSerializer
 
 
-@method_decorator(csrf_exempt, name="dispatch")
 class ContactUsCreateView(generics.CreateAPIView):
     queryset = ContactUs.objects.all()
     serializer_class = ContactUsSerializer
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super(ContactUsCreateView, self).dispatch(*args, **kwargs)
